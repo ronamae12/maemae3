a4683f42f6b8686cf83033dfee69d332d43de7cd
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-09-11 22:07:37 +0800

    completed variables section

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNumb = 10; 
+let secondFavNumb = 5; 
+
+// Greater than
+console.log(firstFavNumb > secondFavNumb);
+
+// Less than
+console.log(firstFavNumb < secondFavNumb);
+
+// Greater than or equal to
+console.log(firstFavNumb >= secondFavNumb);
+
+// Less than or equal to
+console.log(firstFavNumb <= secondFavNumb);
+
+// Strict equality (===)
+console.log(firstFavNumb === secondFavNumb);
+
+// Loose equality (==)
+console.log(firstFavNumb == secondFavNumb);
+
+// Strict inequality (!==)
+console.log(firstFavNumb !== secondFavNumb);
+
+// Loose inequality (!=)
+console.log(firstFavNumb != secondFavNumb);
+
+// AND operator
+console.log(firstFavNumb > 0 && secondFavNumb > 0);
+
+// OR operator
+console.log(firstFavNumb > 0 || secondFavNumb > 10);
+
+// NOT operator
+console.log(!(firstFavNumb > secondFavNumb));
