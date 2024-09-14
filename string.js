342605069a86aa55479779ec7afb156251fd993b
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-09-14 20:48:24 +0800

    completed variables section

---
+++ string.js
@@ -0,0 +1,18 @@
+let favActorFirstName = "Belle";
+let favActorLastName = "Mariano";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+
+let lowercase = "Maui".toUpperCase(); 
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += ", his best show is Silicon Valley.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
