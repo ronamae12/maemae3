b86e2cd4023eea490a866c73d299ba30c6190714
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-09-15 11:28:26 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let a = parseInt(price);
+console.log("Using parseInt():", e);
+let b = Number(price);
+console.log("Using Number():", f);
+let c = +price;
+console.log("Using + operator:", g);
+
+
+let distance = 150;
+let y = distance.toString();
+console.log("Using toString():", t);
+let x = String(distance);
+console.log("Using String():", u);
+
+
+let weight = "72.5";
+let z = parseFloat(weight);
+console.log("Using parseFloat():", v);
